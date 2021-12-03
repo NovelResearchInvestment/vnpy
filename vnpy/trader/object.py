f5a2fb84a6c18bb1da859769a9cb09fd7fb6a903
Gabriel Feng <gabrielwfeng@gmail.com>
2021-12-03 12:24:49 +0800

    fix: gurantee the date type writing to db in term of varied data type from data source.

--- vnpy/trader/object.py
+++ vnpy/trader/object.py
@@ -35,42 +35,42 @@ class TickData(BaseData):
     datetime: datetime
 
     name: str = ""
-    volume: float = 0
-    turnover: float = 0
-    open_interest: float = 0
-    last_price: float = 0
-    last_volume: float = 0
-    limit_up: float = 0
-    limit_down: float = 0
+    volume: float = 0.0
+    turnover: float = 0.0
+    open_interest: float = 0.0
+    last_price: float = 0.0
+    last_volume: float = 0.0
+    limit_up: float = 0.0
+    limit_down: float = 0.0
 
-    open_price: float = 0
-    high_price: float = 0
-    low_price: float = 0
-    pre_close: float = 0
+    open_price: float = 0.0
+    high_price: float = 0.0
+    low_price: float = 0.0
+    pre_close: float = 0.0
 
-    bid_price_1: float = 0
-    bid_price_2: float = 0
-    bid_price_3: float = 0
-    bid_price_4: float = 0
-    bid_price_5: float = 0
+    bid_price_1: float = 0.0
+    bid_price_2: float = 0.0
+    bid_price_3: float = 0.0
+    bid_price_4: float = 0.0
+    bid_price_5: float = 0.0
 
-    ask_price_1: float = 0
-    ask_price_2: float = 0
-    ask_price_3: float = 0
-    ask_price_4: float = 0
-    ask_price_5: float = 0
+    ask_price_1: float = 0.0
+    ask_price_2: float = 0.0
+    ask_price_3: float = 0.0
+    ask_price_4: float = 0.0
+    ask_price_5: float = 0.0
 
-    bid_volume_1: float = 0
-    bid_volume_2: float = 0
-    bid_volume_3: float = 0
-    bid_volume_4: float = 0
-    bid_volume_5: float = 0
+    bid_volume_1: float = 0.0
+    bid_volume_2: float = 0.0
+    bid_volume_3: float = 0.0
+    bid_volume_4: float = 0.0
+    bid_volume_5: float = 0.0
 
-    ask_volume_1: float = 0
-    ask_volume_2: float = 0
-    ask_volume_3: float = 0
-    ask_volume_4: float = 0
-    ask_volume_5: float = 0
+    ask_volume_1: float = 0.0
+    ask_volume_2: float = 0.0
+    ask_volume_3: float = 0.0
+    ask_volume_4: float = 0.0
+    ask_volume_5: float = 0.0
 
     localtime: datetime = None
 
@@ -91,13 +91,13 @@ class BarData(BaseData):
 
     interval: Interval = None
     name: str = ""
-    volume: float = 0
-    turnover: float = 0
-    open_interest: float = 0
-    open_price: float = 0
-    high_price: float = 0
-    low_price: float = 0
-    close_price: float = 0
+    volume: float = 0.0
+    turnover: float = 0.0
+    open_interest: float = 0.0
+    open_price: float = 0.0
+    high_price: float = 0.0
+    low_price: float = 0.0
+    close_price: float = 0.0
 
     def __post_init__(self):
         """"""
@@ -118,9 +118,9 @@ class OrderData(BaseData):
     type: OrderType = OrderType.LIMIT
     direction: Direction = None
     offset: Offset = Offset.NONE
-    price: float = 0
-    volume: float = 0
-    traded: float = 0
+    price: float = 0.0
+    volume: float = 0.0
+    traded: float = 0.0
     status: Status = Status.SUBMITTING
     datetime: datetime = None
     reference: str = ""
@@ -160,8 +160,8 @@ class TradeData(BaseData):
     direction: Direction = None
 
     offset: Offset = Offset.NONE
-    price: float = 0
-    volume: float = 0
+    price: float = 0.0
+    volume: float = 0.0
     datetime: datetime = None
 
     def __post_init__(self):
@@ -181,11 +181,11 @@ class PositionData(BaseData):
     exchange: Exchange
     direction: Direction
 
-    volume: float = 0
-    frozen: float = 0
-    price: float = 0
-    pnl: float = 0
-    yd_volume: float = 0
+    volume: float = 0.0
+    frozen: float = 0.0
+    price: float = 0.0
+    pnl: float = 0.0
+    yd_volume: float = 0.0
 
     def __post_init__(self):
         """"""
@@ -202,8 +202,8 @@ class AccountData(BaseData):
 
     accountid: str
 
-    balance: float = 0
-    frozen: float = 0
+    balance: float = 0.0
+    frozen: float = 0.0
 
     def __post_init__(self):
         """"""
@@ -243,7 +243,7 @@ class ContractData(BaseData):
     net_position: bool = False      # whether gateway uses net position volume
     history_data: bool = False      # whether gateway provides bar history data
 
-    option_strike: float = 0
+    option_strike: float = 0.0
     option_underlying: str = ""     # vt_symbol of underlying contract
     option_type: OptionType = None
     option_expiry: datetime = None
@@ -322,7 +322,7 @@ class OrderRequest:
     direction: Direction
     type: OrderType
     volume: float
-    price: float = 0
+    price: float = 0.0
     offset: Offset = Offset.NONE
     reference: str = ""
 
